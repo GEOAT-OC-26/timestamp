bedf481c2388a3738851dc8b6449095ac34dd19b
ctrujillo26 <ctrujillo@thecodecampus.com>
2016-02-10 19:42:48 +0000

    changed date

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/:datestring', function(req,res) {
   if(myDate.isValid()) {
     res.json({
       unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
+      natural: myDate.format("MM D, YYYY")
     });
   } else {
     res.json({
