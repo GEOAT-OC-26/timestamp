f4e7b330eb091e965f01dbcf090ce1501c709c97
ctrujillo26 <ctrujillo@thecodecampus.com>
2016-02-11 04:55:42 +0000

    update to center of text and javascript file

--- static/js/function.js
+++ static/js/function.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#submit").click(function(){
+    $("#submit").on("click", function(){
         $("#submit").hide();
         $("#label").hide();
         $("#show").html("<h5>Thank You</h5>");
