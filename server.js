7843c098ea8eb16b023f6d83b8fcda171f5a8e06
ctrujillo26 <ctrujillo@thecodecampus.com>
2016-02-11 00:59:35 +0000

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(port, function(){
   console.log("Listening on port: " + port);
 });
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(path.join(__dirname + '/static')));
 
 app.get('/', function(req, res) {
   var fileName = path.join(__dirname, "static", '/index.html');
